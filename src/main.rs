bd20b5cd7807631c3d3b605a01d51688760a3d44
Yehor Smoliakov <yehors@ukr.net>
2025-03-25 02:28:50 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,16 @@ use image::{DynamicImage, GenericImageView, Rgba, imageops::FilterType};
 use ndarray::{Array4, ArrayBase};
 
 fn image_to_yolo(original_image: &DynamicImage) -> Array4<f32> {
-    let mut input = ArrayBase::zeros((1, 640, 640, 3));
+    let mut input = ArrayBase::zeros((1, 3, 640, 640));
 
     let image = original_image.resize_exact(640, 640, FilterType::CatmullRom);
     for (x, y, Rgba([r, g, b, _])) in image.pixels() {
         let x = x as usize;
         let y = y as usize;
 
-        input[[0, y, x, 0]] = (r as f32) / 255.;
-        input[[0, y, x, 1]] = (g as f32) / 255.;
-        input[[0, y, x, 2]] = (b as f32) / 255.;
+        input[[0, 0, y, x]] = (r as f32) / 255.;
+        input[[0, 1, y, x]] = (g as f32) / 255.;
+        input[[0, 2, y, x]] = (b as f32) / 255.;
     }
 
     input
